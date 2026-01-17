6338d196a4ca3f12085fe6ae6e92f5fb1b54edce
Mateusz Mateusz <mateusz.iw@gmail.com>
2026-01-17 16:40:37 +0100

    Add RefundResponseDto and RefundData records for refunds
    Introduced ApiClientPrzelewy24.Objects namespace with RefundResponseDto and RefundData records to model Przelewy24 refund response. Includes XML documentation for properties such as refund UUID and status.

---
+++ Objects/RefundResponseDto.cs
@@ -0,0 +1,23 @@
+namespace ApiClientPrzelewy24.Objects
+{
+    /// <summary>
+    /// Odpowiedü z Przelewy24 po øπdaniu zwrotu
+    /// </summary>
+    public record RefundResponseDto
+    {
+        public RefundData? Data { get; init; }
+    }
+
+    public record RefundData
+    {
+        /// <summary>
+        /// UUID zwrotu
+        /// </summary>
+        public string? RefundsUuid { get; init; }
+        
+        /// <summary>
+        /// Status zwrotu
+        /// </summary>
+        public string? Status { get; init; }
+    }
+}
