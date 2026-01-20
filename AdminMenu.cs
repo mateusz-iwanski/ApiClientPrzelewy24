260657cc3ade50dbd8a7594b5975eec366b0ff10
Mateusz Mateusz <mateusz.iw@gmail.com>
2026-01-20 18:37:41 +0100

    Add admin menu for Przelewy24 settings in PaymentGateway
    Introduced an AdminMenu class to the OrchardCore.PaymentGateway module, adding a localized "Przelewy24" settings link under Configuration > Settings in the admin menu. Access to this menu item is restricted to users with the ManagePaymentGateway permission.

---
+++ AdminMenu.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Localization;
+using OrchardCore.Navigation;
+using OrchardCore.Security.Permissions;
+using System.Threading.Tasks;
+
+namespace OrchardCore.PaymentGateway;
+
+public class AdminMenu : INavigationProvider
+{
+    private readonly IStringLocalizer S;
+
+    public AdminMenu(IStringLocalizer<AdminMenu> localizer) => S = localizer;
+
+    public ValueTask BuildNavigationAsync(string name, NavigationBuilder builder)
+    {
+        if (!string.Equals(name, "admin", StringComparison.OrdinalIgnoreCase))
+        {
+            return ValueTask.CompletedTask;
+        }
+
+        builder.Add(S["Configuration"], cfg => cfg
+            .Add(S["Settings"], settings => settings
+                .Add(S["Przelewy24"], S["Przelewy24"], item => item
+                    .Action("Index", "Admin", new { area = "OrchardCore.Settings", groupId = "Przelewy24" })
+                    .Permission(Permissions.ManagePaymentGateway)
+                    .LocalNav()
+                )));
+
+        return ValueTask.CompletedTask;
+    }
+}
